646682eefe791cf9d062b41dc37f3580cba768a7
Francois Wouts <f@zenc.io>
2018-03-31 21:22:43 +1100

    Fix indentation in tests

--- src/parser.spec.ts
+++ src/parser.spec.ts
@@ -4,22 +4,22 @@ import { parse } from "./parser";
 test("parser", () => {
   expect(() =>
     parse(`
-  type a = b
+type a = b
   `),
   ).toThrow("Syntax error (3:2): missing ';' at '<EOF>'.");
   expect(() =>
     parse(`
-  type a = b;
-  type b =
+type a = b;
+type b =
   `),
   ).toThrow(
     "Syntax error (4:2): mismatched input '<EOF>' expecting {'|', '{', NAME}.",
   );
   expect(() =>
     parse(`
-  type a = 123
+type a = 123
   `),
-  ).toThrow("Syntax error (2:11): token recognition error at: '1'.");
+  ).toThrow("Syntax error (2:9): token recognition error at: '1'.");
   expect(() =>
     parse(`
 export type a = string;
@@ -32,14 +32,14 @@ export type a = string;
   });
   expect(
     parse(`
-  type a = b | string | int;
+type a = b | string | int;
 
-  type b = {
-    field1: string;
-    field2: a;
-  };
+type b = {
+  field1: string;
+  field2: a;
+};
 
-  type c = string;
+type c = string;
   `),
   ).toEqual({
     typeDefs: [
