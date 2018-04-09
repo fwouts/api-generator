14e9701259df762d44af802d9252823b5d427ffc
Francois Wouts <f@zenc.io>
2018-04-10 09:29:08 +1000

    Add test for valid TypeScript compilation

--- src/generators/typescript.spec.ts
+++ src/generators/typescript.spec.ts
@@ -1,4 +1,5 @@
 import "jest";
+import ts from "typescript";
 import { parse } from "../parser";
 import { resolve } from "../resolver";
 import { generateTypeScript } from "./typescript";
@@ -95,6 +96,39 @@ export interface AuthRequired {
       },
     },
   });
+});
+
+test("generator creates enforceable types", () => {
+  if (API.kind !== "success") {
+    throw new Error(`Invalid test data:\n${API.errors.join("\n")}`);
+  }
+  const generated = generateTypeScript(API.definedEndpoints, API.definedTypes);
+  const apiSourceFile = generated.children["api.ts"];
+  expect(apiSourceFile).toBeTruthy();
+  if (apiSourceFile.kind !== "file") {
+    throw expect(apiSourceFile.kind).toEqual("file");
+  }
+  const testSource =
+    apiSourceFile.content +
+    `
+    let validRequest: CreateUserRequest = {
+      name: "Hello",
+      roles: [],
+    };
+
+    let requestMissingRoles: CreateUserRequest = {
+      name: "test",
+    };
+
+    let responseMissingMessage: CreateUserResponse = {
+      status: "error",
+    };
+  `;
+  const { errors } = compile(testSource);
+  expect(errors).toEqual([
+    "Type '{ name: string; }' is not assignable to type 'CreateUserRequest'.",
+    "Type '{ status: \"error\"; }' is not assignable to type 'CreateUserResponse'.",
+  ]);
 });
 
 test("generator with client", () => {
@@ -353,3 +387,55 @@ export async function listUsers(headers: AuthOptional): Promise<ListUsersRespons
     },
   });
 });
+
+function compile(source: string) {
+  const defaultCompilerHost = ts.createCompilerHost({});
+  const tsProgram = ts.createProgram(
+    ["source.ts"],
+    {
+      strict: true,
+      lib: ["es6"],
+    },
+    {
+      ...defaultCompilerHost,
+      getSourceFile(
+        fileName: string,
+        languageVersion: ts.ScriptTarget,
+        onError?: (message: string) => void,
+      ): ts.SourceFile | undefined {
+        if (fileName === "source.ts") {
+          return ts.createSourceFile(fileName, source, languageVersion);
+        } else {
+          return defaultCompilerHost.getSourceFile(
+            fileName,
+            languageVersion,
+            onError,
+          );
+        }
+      },
+      fileExists: (fileName: string) =>
+        fileName === "source.ts" || defaultCompilerHost.fileExists(fileName),
+      readFile: (fileName: string) =>
+        fileName === "source.ts"
+          ? source
+          : defaultCompilerHost.readFile(fileName),
+    },
+  );
+  const diagnostics = ts.getPreEmitDiagnostics(tsProgram);
+  const errors = diagnostics
+    .filter((diagnostic) => {
+      return diagnostic.file && diagnostic.file.fileName === "source.ts";
+    })
+    .map((diagnostic) => {
+      if (typeof diagnostic.messageText === "string") {
+        return diagnostic.messageText;
+      } else {
+        return diagnostic.messageText.messageText;
+      }
+    });
+  const transpiled = ts.transpileModule(source, {}).outputText;
+  return {
+    transpiled,
+    errors,
+  };
+}
